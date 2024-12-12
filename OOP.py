ef02a698138a3ee9e633c0a720abfa8d598cd963
Tamjidthecoder <tamjidcool797@gmail.com>
2024-12-13 00:07:13 +0600

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,12 @@
+class myhhero:
+    def __init__(self,name,country):
+        self.name=name
+        self.country=country
+ 
+a=myhhero('ashrafi','khulna')
+b=myhhero('tamjid','bangladesh')
+
+print(a.name)
+print(a.country)
+print(b.name)
+print(b.country)
